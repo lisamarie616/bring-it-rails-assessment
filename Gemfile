4403e3713054d0a12ce27cda1b8562f62fa79fbc
lisamarie616 <lisamarie616@gmail.com>
2016-03-14 23:08:40 -0700

    add bootstrap and bootswatch gems, create navbar, messages partial

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'bootswatch-rails'
 gem 'bootstrap_form'
 
 gem 'devise'
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,25 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @hosted_events = current_user.hosted_events
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end

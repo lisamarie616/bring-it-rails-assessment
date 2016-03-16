47dee2ded2167f1ffdafa7dd1d52e85ee400a2a2
lisamarie616 <lisamarie616@gmail.com>
2016-03-15 18:32:52 -0700

    add first_name and last_name fields to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,8 @@ class User < ActiveRecord::Base
     end
   end
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313213112) do
+ActiveRecord::Schema.define(version: 20160316011858) do
 
   create_table "event_items", force: :cascade do |t|
     t.integer  "event_id"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20160313213112) do
     t.datetime "updated_at",                       null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
